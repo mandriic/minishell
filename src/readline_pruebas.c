f8e681fc965dfa229c54118f4c3497eadc73f9ae
mandriic <kellaxxx@gmail.com>
2022-06-27 20:19:56 +0200

    Parsing + spliting

--- src/readline_pruebas.c
+++ src/readline_pruebas.c
@@ -13,19 +13,25 @@ char **spliting(char *wololoco, int *type)
 	}
 	// if (num_pipes == 0)
 	// 	return(&wololoco);
-	separ = malloc(sizeof(char *) * num_pipes +1);
-	i = 0;
-	while(wololoco[i] != '\0')
+	separ = malloc(sizeof(char *) * num_pipes + 2);
+	i = 1;
+	while(wololoco[i - 1] != '\0')
 	{
-		if (type[i] == 3)
+		if (type[i - 1] == 3)
 		{
-			separ[i2++] = ft_substr(wololoco, start, i - 1);
-			start = i + 1;
-			// write(1, "1",1);
+			separ[i2++] = ft_substr(wololoco, start, i - start - 1);
+			start = i;
+			// printf("%d\n", i);
+		printf("%s\n", separ[i2 - 1]);
+
 		}
 		i++;
 	}
-	return(separ);
+	separ[i2++] = ft_substr(wololoco, start, i  - start);
+	printf("%s\n", separ[i2 - 1]);
+
+	separ[i2] = NULL;
+ 	return(separ);
 }
 int main(void)
 {
@@ -72,13 +78,20 @@ int main(void)
 					type[i] = 0;
 				type[i] = 2;
 			}
-			else if (wololo[i] == pipe[0])
+			else if (wololo[i] == pipe[0] && wololo[i + 1] != '\0')
 				type[i] = 3;
+			else if (wololo[i] == pipe[0] && wololo[i + 1] == '\0')
+				type[i] = 4;
 			else
 				type[i] = 0;
 		}
-		split = spliting(wololo_cop, type);	
-		printf("%s\n", split[1]);
+
+		split = spliting(wololo_cop, type);
+		// i = -1;
+			// printf("%s\n", split[0]);
+
+		// while (split[++i])
+		// 	printf("%s\n", split[i]);
 		i = 0;
 		finline = malloc(sizeof(char) * line_len + 1);
 		int i2 = 0;
@@ -95,9 +108,13 @@ int main(void)
 		}
 		finline[i2] = '\0';
 		// printf("%s\n", finline);
-		// printf("%s\n", wololo_cop);
-	// 	for(i = 0; i < line_len; i++)
-	// 		printf("%d\n", type[i]);
+		 // printf("%s\n", wololo_cop);
+		// for(i = 0; i < line_len; i++)
+		// 	printf("%d\n", type[i]);
+		i = -1;
+		// while (split[++i] != NULL)
+		// 	free(split[i]);
+		// free(split);
 	}
 
 		return (0);
