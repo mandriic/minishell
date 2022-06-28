91fb7d79ec63a19059f86bd9535f372cebaa038f
mandriic <kellaxxx@gmail.com>
2022-06-28 10:09:43 +0200

    if 0 pipes, other malloc

--- src/readline_pruebas.c
+++ src/readline_pruebas.c
@@ -1,18 +1,25 @@
 #include "../inc/minishell.h"
-char **spliting(char *wololoco, int *type)
+size_t ft_numpipes(char *wololoco, int *type)
 {
-	int i = -1;
-	int i2 = 0;
-	int num_pipes = 0;
-	char **separ;
-	int start = 0;
+	int num_pipes;
+	int	i;
+
+	i = -1;
+	num_pipes = 0;
 	while(wololoco[++i])
 	{
 		if (type[i] == 3)
 			num_pipes++;
 	}
-	// if (num_pipes == 0)
-	// 	return(&wololoco);
+	return (num_pipes);
+}
+char **spliting(char *wololoco, int *type, size_t num_pipes)
+{
+	int i = -1;
+	int i2 = 0;
+	char **separ;
+	int start = 0;
+	
 	separ = malloc(sizeof(char *) * num_pipes + 2);
 	i = 1;
 	while(wololoco[i - 1] != '\0')
@@ -23,13 +30,11 @@ char **spliting(char *wololoco, int *type)
 			start = i;
 			// printf("%d\n", i);
 		printf("%s\n", separ[i2 - 1]);
-
 		}
 		i++;
 	}
 	separ[i2++] = ft_substr(wololoco, start, i  - start);
 	printf("%s\n", separ[i2 - 1]);
-
 	separ[i2] = NULL;
  	return(separ);
 }
@@ -44,11 +49,7 @@ int main(void)
 	char *finline;
 	char d_cuotes;
 	char **split;
-
 	cuotes  = "'";
-	d_cuotes = '"';
-	char *pipe;
-	pipe = "|";
 	int i_cuot;
 	i_cuot = 0;
 	while (1)
@@ -70,7 +71,7 @@ int main(void)
 					type[i] = 0;
 				type[i] = 1;
 			}
-			else if (wololo[i] == d_cuotes)
+			else if (wololo[i] == '"')
 			{
 				i_cuot = i_cuot + 2;
 				type[i] = 2;
@@ -78,15 +79,23 @@ int main(void)
 					type[i] = 0;
 				type[i] = 2;
 			}
-			else if (wololo[i] == pipe[0] && wololo[i + 1] != '\0')
+			else if (wololo[i] == '|' && wololo[i + 1] != '\0')
 				type[i] = 3;
-			else if (wololo[i] == pipe[0] && wololo[i + 1] == '\0')
+			else if (wololo[i] == '|' && wololo[i + 1] == '\0')
 				type[i] = 4;
 			else
 				type[i] = 0;
 		}
-
-		split = spliting(wololo_cop, type);
+		size_t num_pipes = 0;
+		num_pipes = ft_numpipes(wololo_cop, type);
+		if (num_pipes)
+			split = spliting(wololo_cop, type, num_pipes);
+		else
+		{
+			split = malloc(sizeof(char *) * 2);
+			split[0] = wololo_cop;
+		}
+		printf("%s\n", split[0]);
 		// i = -1;
 			// printf("%s\n", split[0]);
 
