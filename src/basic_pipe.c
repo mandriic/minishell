378c0384080657218ee8aad8524c3a4fe31a9596
PepeGF <gran.pepe@gmail.com>
2022-07-07 21:47:30 +0200

    very basic pipe working

--- src/basic_pipe.c
+++ src/basic_pipe.c
@@ -29,6 +29,13 @@ int	ft_parent(int *fd)
 	return (y);
 }
 
+void	ft_error_exit(char *err_msg)
+{
+	printf("%s\n", err_msg);
+	perror("");
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, char *argv[], char *envp[])
 {
 	(void)argc;
@@ -41,10 +48,66 @@ int main(int argc, char *argv[], char *envp[])
 		NULL
 	};
 	char *cmd2[] = {
-		"grep",
-		"txt",
+		"wc",
 		NULL
 	};
+	char	outfile[] = "wololo.txt";
+
+	int	id;
+	int	id2;
+	int	fd1[2]; //0 leer, 1 escribir
+	int	fd2;
+
+	if (pipe(fd1) == -1)
+		ft_error_exit("Error pipe");
+	id = fork();
+	if (id == -1)
+		ft_error_exit("Error fork");
+	if (id == 0)
+	{
+
+		if (close(fd1[0]) == -1)
+			ft_error_exit("Error close");
+		if (dup2(fd1[1], STDOUT_FILENO) == -1)
+			ft_error_exit("Error dup");
+		if (close(fd1[1]) == -1)
+			ft_error_exit("Error close");
+		if (execve("/bin/ls", cmd1, envp) == -1)
+			ft_error_exit("Error comando ejecución");
+	}
+	else
+	{
+		close(fd1[1]);
+		// close(fd1[0]);
+		id2 = fork();
+		if (id2 == 0)
+		{
+
+			//if hay redirección
+			fd2 = open(outfile, O_WRONLY | O_CREAT, 0777);
+			dup2(fd1[0], STDIN_FILENO);
+			close(fd1[0]);
+			dup2(fd2, STDOUT_FILENO);
+			write(1, "XXXX\n", 5);
+			//end if
+			if (execve("/usr/bin/wc", cmd2, envp) == -1)
+				write(2, "Oh oh\n", 6);
+		}
+		else
+		{
+			close(fd1[0]);
+		}
+		wait(NULL);
+		wait(NULL);
+
+	}
+
+
+
+	return (0);
+}
+
+/* 
 (void)cmd1;
 (void)cmd2;
 
@@ -81,7 +144,4 @@ int main(int argc, char *argv[], char *envp[])
 		}
 		return (0);
 	}
-
-
-	return (0);
-}
+ */
