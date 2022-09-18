36a484548d8a1b682f27b62d1f17492bd4d7eeaa
mandriic <kellaxxx@gmail.com>
2022-09-18 22:26:22 +0200

    norm cleaning... 10% fn_pars.c fn_pip_splt_trim.c

--- src/fn_pip_splt_trim.c
+++ src/fn_pip_splt_trim.c
@@ -17,7 +17,6 @@ char    **spliting(char *wololoco, int *type, size_t num_pipes, t_vars *vars)
 	int i = -1;
 	int i2 = 0;
 	char **separ;
-	// char **separ2;
 	int start = 0;
 	
 	separ = malloc(sizeof(char *) * (num_pipes + 2));
@@ -28,80 +27,54 @@ char    **spliting(char *wololoco, int *type, size_t num_pipes, t_vars *vars)
 		{
 			separ[i2++] = ft_substr(wololoco, start, i - start - 1);
 			start = i;
-			// printf("%d\n", i);
-		// printf("%s\n", separ[i2 - 1]);
 		}
 		i++;
-		// printf("sep%s\n", separ[i2 - 1]);
-		
 	}
 	separ[i2++] = ft_substr(wololoco, start, i  - start);
 	separ[i2] = NULL;
-	// int i3;
-	// int *sub_type;
-
 	ft_triming(separ, num_pipes, vars, 0);
-	// while(--i2 != 0)
-	// {
-	// 	free(separ[i2]);
-	// 	separ[i2] = NULL;
-	// }
-	// free(separ);
-	// for(i= 0; temp[i] != '\0'; i++)
-	// 	printf("%s\n", temp[i]);
  	return(separ);
+}
+
+void	ft_subtrim(char **temp, int *i, int *i2, t_vars *vars)
+{
+	int	*sub_type;
+	char	**separ;
+	int	i3;
+
+	i3 = 0;
+	separ = &vars->line;
+	sub_type = ft_mask(temp[0], vars);
+	while (temp[0][++*i2])
+	{
+		if (temp[0][*i2] == ' ' && temp[0][*i2 + 1] == ' '\
+			&& sub_type[*i2] != 5 && sub_type[*i2] != 6)
+			continue ;
+		separ[*i][i3] = temp[0][*i2];
+		i3++;
+	}
+	free(sub_type);
+	free(temp[0]);
+	separ[*i][i3] = '\0';
 }
 
 void    ft_triming(char **separ, size_t num_pipes, t_vars *vars, int one_comand)
 {
 	int	i;
 	int	i2;
-	int	i3;
 	char **temp;
-	int *sub_type;
 
 	temp = malloc(sizeof(char *) * (num_pipes + 2));
-	// printf("sizeof%lu\n", sizeof(separ));
 	i = -1;
-		while (separ[++i])
-		{
-			// printf("i%d\n", i);
-			// printf("separ%s\n", separ[i]);
-			temp[0] = ft_strtrim(separ[i], " ");
-			sub_type = ft_mask(temp[0], vars);
-			// free(separ[i]);
-			i2 = -1;
-			i3 = 0;
-			while (temp[0][++i2])
-			{
-				// printf("%d\n",sub_type[i]);
-				// printf("check\n");
-// 				sleep(1);
-				if (temp[0][i2] == ' ' && temp[0][i2 + 1] == ' ' && sub_type[i2] != 5 && sub_type[i2] != 6)
-					continue ;
-				separ[i][i3] = temp[0][i2];
-				i3++;
-			}
-			free(sub_type);
-			free(temp[0]);
-			separ[i][i3] = '\0';
+	while (separ[++i])
+	{
+		temp[0] = ft_strtrim(separ[i], " ");
+		i2 = -1;
+		ft_subtrim(temp, &i, &i2, vars);
 	}
 	free(temp);
-	// int ii = -1;
-	// while(++ii != i)
-	// 	free(mem[ii]);
-	// free(mem[0]);
-// 	ft_free_double_arr(mem);
-	// temp[i] = NULL;
-	// printf("i2%d\n",i);
 	if (one_comand)
-	{
-
 		vars->split = separ;
-	}
-		// vars->split = separ;
-	
-	// return(separ);
 }
 
 size_t  ft_numpipes(char *wololoco, int *type)
