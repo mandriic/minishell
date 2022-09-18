37c5bc2336d031badf200b9b3e7c2f03bc0d216a
mandriic <kellaxxx@gmail.com>
2022-09-18 14:37:40 +0200

    clean 2 norm  fn_cleaning

--- src/fn_main_readline.c
+++ src/fn_main_readline.c
@@ -1,8 +1,8 @@
 #include "../inc/minishell.h"
 
-void    ft_readline(void)
+void	ft_readline(void)
 {
-	t_vars vars;
+	t_vars  vars;
 
 	vars = (t_vars){};
 	vars.split = NULL;
@@ -10,180 +10,124 @@ void    ft_readline(void)
 	vars.line = NULL;
 	vars.quotes  = "'";
 	ft_submain(&vars);
-// 		system("leaks minishell");
 }
 
-void    ft_submain(t_vars *vars)
+void	ft_line_exist(t_vars *vars)
+{
+	vars->num_pipes = ft_numpipes(vars->line, vars->type);
+	if (vars->num_pipes)
+		vars->split = spliting(vars->line, vars->type, vars->num_pipes, vars);
+	else
+	{
+		free(vars->type);
+		vars->type = NULL;
+		ft_triming(&vars->line, 0, vars, 1);
+	}
+	ft_lst_cmd(vars);
+}
+
+void	ft_submain(t_vars *vars)
 {
 	while (1)
 	{
 		vars->line = readline("Minishell $ ");
-		// if (ft_strlen(vars.line) == 0)
-		// {
-		// 	write(1, "exit\n", 5);
-		// 	free(vars.line);
-		// 	exit (0);
-		// }
 		if (!ft_strncmp ("exit", vars->line, ft_strlen(vars->line)))
 		{
 			write(1, "exit\n", 5);
-			free(vars->type);
-			free(vars->line);
 			exit (0);
 		}
 		if (ft_pre_check(vars))
 			continue ;
-		add_history(vars->line); //solo añadir si es válido
-		// line_cop = ft_strdup(wololo);
+		add_history(vars->line);
 		vars->line_len = ft_strlen(vars->line);
-		// printf("%zu\n", line_len	);sad
-		vars->type = ft_mask(vars->line, vars); //free
+		vars->type = ft_mask(vars->line, vars);
 		if (vars->type != NULL)
-		{
-			vars->num_pipes = ft_numpipes(vars->line, vars->type);
-			if (vars->num_pipes)
-				vars->split = spliting(vars->line, vars->type, vars->num_pipes, vars);
-			else
-			{
-				free(vars->type);
-				vars->type = NULL;
-				ft_triming(&vars->line, 0, vars, 1);
-				// vars->split[1] = NULL;
-			}
-			ft_lst_cmd(vars);
-		}
+			ft_line_exist(vars);
 		ft_end_of_cicle(vars);
 	}
 }
 
-void    ft_end_of_cicle(t_vars *vars)
+void	ft_end_of_cicle(t_vars *vars)
 {
-
-		
-		free(vars->line);
-		vars->line = NULL;
-		free(vars->type);
-		// free(vars.split);
-		vars->type = NULL;
-		// free(vars->temp);
-		// vars->temp = NULL;
-
+	free(vars->line);
+	vars->line = NULL;
+	free(vars->type);
+	vars->type = NULL;
 }
 
-int ft_pre_check(t_vars *vars)
+int	ft_pre_check(t_vars *vars)
 {
-	int	alpha;
+	int alpha;
+
 	vars->i = -1;
 	alpha = 0;
-	while(vars->line[++vars->i])
+	while (vars->line[++vars->i])
 	{
 		if (ft_isalnum(vars->line[vars->i]))
 			alpha++;
 	}
 	if (alpha == 0)
-		return(1);
+		return (1);
 	return (0);
 }
 
-char    *ft_cleaning(char *str)
+void	ft_subcleaning(char *str, int *start, char **clear, char **temp, t_vars *vars)
 {
-	int i;
-	char *cuot;
-	// int i2;
-	char *clear;
-	int start;
-	char *temp;
-
-	
-	cuot = "'";
-	i = -1;
-	// while (str[++i])
-	// 	if (str[i] == '"' || str[i] == cuot[0])
-	// 	{
-			// i2 = ft_strlen(str);
-
-			// clear = malloc(sizeof(char) * (i2 + 1));
-
-			clear = NULL;
-			start = 0;
-			// i = -1;
-			while (str[++i])
-			{
-				if (str[i] == '"' || str[i] == cuot[0])
-				{
-					if (i - start != 0)
-					{
-						if (start == 0)
-							temp = ft_substr(str, start, i - start);
-						else
-							temp = ft_substr(str, start + 1, i - start - 1);
-						clear = ft_acumulate(clear, temp);
-						// free(temp);
-					}
-					start = i;
-					i++;
-					// if(clear != NULL)
-					// {
-					// 	temp = ft_substr(str, start, i - start - 1); // <-
-					// 	clear = ft_acumulate(clear, temp);
-					// 	start = i + 1;
-					// }
-					while (str[i] != '"' && str[i] != cuot[0]  && str[i] != '\0')
-					{
-						
-						i++;
-					}
-					temp = ft_substr(str, start + 1, i - start - 1);
-					start = i;
-					printf("CHEEECK\n");
-
-					printf("cleaning temp .%s.\n", temp);
-					printf("clear befor .%s.\n", clear);
-					clear = ft_acumulate(clear, temp);
-					printf("clean desp .%s.\n", clear);
-
-					if (str[i] == '\0')
-						break ;
-				}
-			}
-			if (start != i && clear != NULL) //start != 0 && 
-			{
-				// if (str[])
-				temp = ft_substr(str, start + 1, i - start - 1); // start + 1
-				printf("error %s\n", temp);
-				clear = ft_acumulate(clear, temp);	
-				// if(temp)
-				// 	free(temp);
-				
-				return (clear);
-			}
-			else if (clear == NULL)
-			{
-				free(clear);
-				free(clear);
-				free(clear);
-				free(clear);
-				free(clear);
-				free(clear);
-				printf("!!!!!!!!!!!!chkkkkk\n");
-				free(clear);
-				free(clear);
-				free(clear);
-				free(clear);
-				return (ft_strdup(str));
-			}
+	if (vars->i - *start != 0)
+		{
+			if (*start == 0)
+				*temp = ft_substr(str, *start, vars->i - *start);
 			else
-			{
-				// free(str);
-				printf("c_clear\n");
-				return (clear);
-			}
-			// while(str[++i])
-			// {
-			// 	if (str[i] == '"' || str[i] == cuot)
-			// 		i2++;
+				*temp = ft_substr(str, *start + 1, vars->i - *start - 1);
+			*clear = ft_acumulate(*clear, *temp);
+		}
+	*start = vars->i;
+	vars->i++;
+	while (str[vars->i] != '"' && str[vars->i] != vars->quotes[0]  && str[vars->i] != '\0')
+		vars->i++;
+	*temp = ft_substr(str, *start + 1, vars->i - *start - 1);
+	*start = vars->i;
 
-			// }
-		// }
-	// return (ft_strdup(str));
+	printf("CHEEECK\n");
+	printf("cleaning temp .%s.\n", *temp);
+	
+	printf("clear befor .%s.\n", *clear);
+	*clear = ft_acumulate(*clear, *temp);
+	printf("clean desp .%s.\n", *clear);
+
+}
+
+char	*ft_cleaning(char *str, t_vars *vars)
+{
+	char	*clear;
+	int		start;
+	char	*temp;
+
+	vars->i = -1;
+	clear = NULL;
+	temp = NULL;
+	start = 0;
+	while (str[++vars->i])
+	{
+		if (str[vars->i] == '"' || str[vars->i] == vars->quotes[0])
+		{
+			ft_subcleaning(str, &start, &clear, &temp, vars);
+			if (str[vars->i] == '\0')
+				break ;
+		}
+	}
+	if (start !=vars->i && clear != NULL)
+	{
+		temp = ft_substr(str, start + 1,vars->i - start - 1);
+		printf("error %s\n", temp);
+		clear = ft_acumulate(clear, temp);	
+		return (clear);
+	}
+	else if (clear == NULL)
+	{
+		free(clear);
+		return (ft_strdup(str));
+	}
+	else
+		return (clear);
 }
