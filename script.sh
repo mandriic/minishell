985aea27ce33c6ae235e3cfedb1f7f140daee8bb
mandriic <kellaxxx@gmail.com>
2022-12-15 19:42:25 +0100

    Mod docker image for valgrind

--- script.sh
+++ script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cat /home/sshcls/.ssh/id_rsa_shared.pub >> /home/sshcls/.ssh/authorized_keys
+chmod 600 /home/sshcls/.ssh/authorized_keys
+chown -R sshcls:sshcls /home/sshcls/.ssh/
 service ssh start
 service vsftpd start
 
