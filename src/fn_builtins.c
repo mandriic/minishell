76979e42d7604ddd2980595cd6343dd9434cb3ed
Mykola Andriichuk <mandriic@c1r13s2.42malaga.com>
2023-04-02 18:03:22 +0200

    echo + pwd

--- src/fn_builtins.c
+++ src/fn_builtins.c
@@ -2,19 +2,42 @@
 
 int	ft_echo(t_vars *vars)
 {
-    printf("echo builtin %s\n", vars->cmd_list->cmd[0]);
+    int i;
+
+    i = 1;
+    if (ft_strncmp(vars->cmd_list->cmd[1], "-n", 2) == 0)
+        i++;
+    while (vars->cmd_list->cmd[i] != NULL)
+    {
+        printf ("%s", vars->cmd_list->cmd[i++]);
+        if (vars->cmd_list->cmd[i + 1] != NULL && ft_strncmp(vars->cmd_list->cmd[1], "-n", 2) != 0)
+            write(1, " ", 1);
+    }
+    if (ft_strncmp(vars->cmd_list->cmd[1], "-n", 2) != 0)
+        printf("\n");
     return(1);
 }
 
 int	ft_cd(t_vars *vars)
 {
     printf("cd builtin\n");
+    // printf("TEST\n");
     return(1);
 }
 
 int	ft_pwd(t_vars *vars)
 {
+    int i;
+
+    i = 0;
     printf("pwd builtin\n");
+    while (vars->env_var[i] != NULL)
+    {
+        if (ft_strncmp(vars->env_var[i], "PWD=", 4) == 0)
+            printf("%s\n", vars->env_var[i] + 4);
+        i++;
+    }
+    // ft_print_dp(vars->env_var, "ENVVAR");
     return(1);
 }
 
