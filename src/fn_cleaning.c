5df5e528931931aa741af1ad21898171a3791278
mandriic <kellaxxx@gmail.com>
2023-08-13 18:58:25 +0200

    FIXED! d cuotes

--- src/fn_cleaning.c
+++ src/fn_cleaning.c
@@ -29,8 +29,11 @@ void	ft_subcleaning(char *str, char **clear, t_vars *vars, int *type)
 	}
 	vars->start2 = vars->i2;
 	vars->i2++;
-	while (str[vars->i2] != '\0' && type[vars->i2] != 1 && type[vars->i2] != 2)
+	while(str[vars->i2] != '\0' && type[vars->i2] != 1 && type[vars->i2] != 2)
+	{
 		vars->i2++;
+	}
+
 	if (!vars->temp || str[vars->i2 + 1] == '\0')
 	{
 		// printf("vars->temp is null\n");
@@ -55,7 +58,27 @@ int	ft_clear_quot(char *str, char **clear, t_vars *vars, int *type)
 	}
 	return (0);
 }
+char *ft_del_quot(char *str, int *type, t_vars *vars)
+{
+	char	*clear;
+	int		i;
+	int		j;
+	clear = malloc(sizeof(char) * ft_strlen(str) + 1);
 
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (type[i]!= 2 && type[i] != 1)
+		{
+			clear[j] = str[i];
+			j++;
+		}
+		i++;
+	}
+	clear[j] = '\0';
+	return (clear);
+}
 char	*ft_cleaning(char *str, t_vars *vars)
 {
 	char	*clear;
@@ -65,23 +88,28 @@ char	*ft_cleaning(char *str, t_vars *vars)
 	clear = NULL;
 	vars->temp = NULL;
 	type = ft_mask(str, vars, 0);
-	while (str[++vars->i2])
-	{
-		if (ft_clear_quot(str, &clear, vars, type))
-			return (NULL);
-		if (str[vars->i2] == '\0')
-			break ;
-	}
-	free(type);
-	if (vars->start2 != vars->i2 && clear != NULL)
-	{
-		vars->temp = ft_substr(str, vars->start2 + 1,
-				vars->i2 - vars->start2 - 1);
-		clear = ft_acumulate(clear, vars->temp);
-		return (clear);
-	}
-	else if (clear == NULL)
-		return (ft_strdup(str));
+	clear = ft_del_quot(str, type, vars);
+	// while (str[++vars->i2])
+	// {
+	// 	if (type[vars->i2] == 2)
+	// 	{
+
+	// 	}
+	// 	if (ft_clear_quot(str, &clear, vars, type))
+	// 		return (NULL);
+	// 	if (str[vars->i2] == '\0')
+	// 		break ;
+	// }
+	// free(type);
+	// if (vars->start2 != vars->i2 && clear != NULL)
+	// {
+	// 	vars->temp = ft_substr(str, vars->start2 + 1,
+	// 			vars->i2 - vars->start2 - 1);
+	// 	clear = ft_acumulate(clear, vars->temp);
+	// 	return (clear);
+	// }
+	// else if (clear == NULL)
+	// 	return (ft_strdup(str));
 	return (clear);
 }
 
