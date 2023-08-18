65fd5142bc7965fdd5a5596a5b5665f86875b764
Sirius <sirius@42madrid.com>
2023-08-18 18:53:30 +0200

    cleaning ft_getenv

--- src/fn_cleaning.c
+++ src/fn_cleaning.c
@@ -3,61 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   fn_cleaning.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mandriic <mandriic@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: mandriic <mandriic@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 16:10:48 by mandriic          #+#    #+#             */
-/*   Updated: 2022/09/30 16:10:51 by mandriic         ###   ########.fr       */
+/*   Updated: 2023/08/18 18:36:10 by mandriic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-void	ft_subcleaning(char *str, char **clear, t_vars *vars, int *type)
-{
-	if (str[vars->i2] == '~')
-	{
-		vars->temp = ft_strdup(getenv("HOME"));
-	}
-	else if (vars->i2 - vars->start2 != 0)
-	{
-		if (vars->start2 == 0)
-			vars->temp = ft_substr(str, vars->start2, vars->i2 - vars->start2);
-		else
-			vars->temp = ft_substr(str, vars->start2 + 1,
-					vars->i2 - vars->start2 - 1);
-		*clear = ft_acumulate(*clear, vars->temp);
-	}
-	vars->start2 = vars->i2;
-	vars->i2++;
-	while(str[vars->i2] != '\0' && type[vars->i2] != 1 && type[vars->i2] != 2)
-	{
-		vars->i2++;
-	}
+// void	ft_subcleaning(char *str, char **clear, t_vars *vars, int *type)
+// {
+// 	if (str[vars->i2] == '~')
+// 	{
+// 		vars->temp = ft_strdup(getenv("HOME"));
+// 	}
+// 	else if (vars->i2 - vars->start2 != 0)
+// 	{
+// 		if (vars->start2 == 0)
+// 			vars->temp = ft_substr(str, vars->start2, vars->i2 - vars->start2);
+// 		else
+// 			vars->temp = ft_substr(str, vars->start2 + 1,
+// 					vars->i2 - vars->start2 - 1);
+// 		*clear = ft_acumulate(*clear, vars->temp);
+// 	}
+// 	vars->start2 = vars->i2;
+// 	vars->i2++;
+// 	while(str[vars->i2] != '\0' && type[vars->i2] != 1 && type[vars->i2] != 2)
+// 		vars->i2++;
+// 	if (!vars->temp || str[vars->i2 + 1] == '\0')
+// 		vars->temp = ft_substr(str, vars->start2 + 1, vars->i2 - vars->start2 - 1);
+// 	vars->start2 = vars->i2;
+// 	*clear = ft_acumulate(*clear, vars->temp);
+// }
 
-	if (!vars->temp || str[vars->i2 + 1] == '\0')
-	{
-		// printf("vars->temp is null\n");
-		vars->temp = ft_substr(str, vars->start2 + 1, vars->i2 - vars->start2 - 1);
-	}
-	vars->start2 = vars->i2;
-	*clear = ft_acumulate(*clear, vars->temp);
-}
-
-int	ft_clear_quot(char *str, char **clear, t_vars *vars, int *type)
-{
-	if (type[vars->i2] == 2 || type[vars->i2] == 1 || str[vars->i2] == '~')
-	{
-		if ((str[vars->i2] == '"' && str[vars->i2 + 1] == '"' && ft_strlen(str) == 2)
-			|| (str[vars->i2] == vars->quotes[0]
-				&& str[vars->i2 + 1] == vars->quotes[0]))
-		{
-			free(type);
-			return (1);
-		}
-		ft_subcleaning(str, clear, vars, type);
-	}
-	return (0);
-}
+// int	ft_clear_quot(char *str, char **clear, t_vars *vars, int *type)
+// {
+// 	if (type[vars->i2] == 2 || type[vars->i2] == 1 || str[vars->i2] == '~')
+// 	{
+// 		if ((str[vars->i2] == '"' && str[vars->i2 + 1] == '"' && ft_strlen(str) == 2)
+// 			|| (str[vars->i2] == vars->quotes[0]
+// 				&& str[vars->i2 + 1] == vars->quotes[0]))
+// 		{
+// 			free(type);
+// 			return (1);
+// 		}
+// 		ft_subcleaning(str, clear, vars, type);
+// 	}
+// 	return (0);
+// }
 char *ft_del_quot(char *str, int *type, t_vars *vars)
 {
 	char	*clear;
@@ -79,6 +73,7 @@ char *ft_del_quot(char *str, int *type, t_vars *vars)
 	clear[j] = '\0';
 	return (clear);
 }
+
 char	*ft_cleaning(char *str, t_vars *vars)
 {
 	char	*clear;
@@ -90,27 +85,6 @@ char	*ft_cleaning(char *str, t_vars *vars)
 	type = ft_mask(str, vars, 0);
 	clear = ft_del_quot(str, type, vars);
 	free(type);
-	// while (str[++vars->i2])
-	// {
-	// 	if (type[vars->i2] == 2)
-	// 	{
-
-	// 	}
-	// 	if (ft_clear_quot(str, &clear, vars, type))
-	// 		return (NULL);
-	// 	if (str[vars->i2] == '\0')
-	// 		break ;
-	// }
-	// free(type);
-	// if (vars->start2 != vars->i2 && clear != NULL)
-	// {
-	// 	vars->temp = ft_substr(str, vars->start2 + 1,
-	// 			vars->i2 - vars->start2 - 1);
-	// 	clear = ft_acumulate(clear, vars->temp);
-	// 	return (clear);
-	// }
-	// else if (clear == NULL)
-	// 	return (ft_strdup(str));
 	return (clear);
 }
 
