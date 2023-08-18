404836593e3dc55e0e30b34d0c4076686eaf1112
mandriic <kellaxxx@gmail.com>
2023-08-18 16:45:24 +0200

    Norn fn_vars.c

--- src/fn_vars.c
+++ src/fn_vars.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fn_vars.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mandriic <mandriic@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: mandriic <mandriic@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 16:15:50 by mandriic          #+#    #+#             */
-/*   Updated: 2022/09/30 16:15:53 by mandriic         ###   ########.fr       */
+/*   Updated: 2023/08/18 16:44:39 by mandriic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_checkif_var(char *str, t_vars *vars)
 {
 	int		*type;
 	char	*acum;
-	// c(str);
+
 	vars->start = 0;
 	type = ft_mask(str, vars, 0);
 	acum = NULL;
@@ -42,35 +42,28 @@ char	*ft_checkif_var(char *str, t_vars *vars)
 
 void	ft_pre_getenv(char *str, char **acum, t_vars *vars)
 {
-	char	lastchar;
-	lastchar = '\0';
+	vars->lastchar = '\0';
 	if (vars->i - vars->start > 0 && (str [vars->i - 1] == ' ' \
-			|| lastchar == '/' || str[vars->i - 1] != vars->quotes[0]) && str[0] != '~')
+			|| vars->lastchar == '/' || str[vars->i - 1] != vars->quotes[0])
+		&& str[0] != '~')
 	{
-		// printf("CHECK1vars->i: %d\n", vars->i);
 		vars->temp = ft_substr(str, vars->start, vars->i - vars->start);
 		*acum = ft_acumulate(*acum, vars->temp);
 	}
 	vars->start = vars->i + 1;
 	vars->i++;
-	// printf("!!!str[%d]: %c\n", vars->i, str[vars->i]);
-	while ( str[vars->i] != '\0' && str[vars->i] != '/' // && str[vars->i] != ' ' // <- ' ' was comented, made error in "$HOME "
+	while (str[vars->i] != '\0' && str[vars->i] != '/'
 		&& str[vars->i] != '"' && str[vars->i] != '$'
-		&& str[vars->i] != vars->quotes[0] && str[vars->i] != '~') 
+		&& str[vars->i] != vars->quotes[0] && str[vars->i] != '~')
 	{
 		if (str[vars->i] == '?' && str[vars->i - 1] == '$')
 			break ;
 		vars->i++;
-		// printf("str5[%d]: %c\n", vars->i, str[vars->i]);
-
-	}// && str[vars->i] != '~'
+	}
 	vars->var = ft_get_env(str + vars->start - 1, vars->i - vars->start, vars);
-	// printf("vars->var: %s\n", vars->var);
 	vars->start = vars->i + 1;
 	*acum = ft_acumulate(*acum, vars->var);
-	// printf("acum: %s\n", *acum);
-	lastchar = str[vars->i];
-	// printf("lastchar: %c\n", lastchar);
+	vars->lastchar = str[vars->i];
 	if (str[vars->i] == '$' && str[vars->i - 1] != ' '
 		&& str[vars->i - 1] != '"')
 		vars->i--;
@@ -79,16 +72,13 @@ void	ft_pre_getenv(char *str, char **acum, t_vars *vars)
 void	ft_checkif_var_subfoo(char *str, char **acum, int *type, t_vars *vars)
 {
 	vars->i = 0;
-	
 	while (1)
 	{
-		// printf("str[%d]: %c\n", vars->i, str[vars->i]);
-		// printf("type[%d]: %d\n", vars->i, type[vars->i]);
 		if (str[vars->i] == '\0')
 			break ;
-		if ((type[vars->i] == 7 && str[vars->i + 1] != ' ' && str[vars->i + 1] != '"')) // && str[vars->i] != '~'&& (type[vars->i] == 6 || type[vars->i] == 0) && 
+		if ((type[vars->i] == 7 && str[vars->i + 1] != ' '
+				&& str[vars->i + 1] != '"'))
 			ft_pre_getenv(str, acum, vars);
-
 		vars->i++;
 	}
 }
