d8125e038b0cd0064968023e5916b3bb6d0494d5
Pedro Reina Ganan <preina-g@c3r8s6.42malaga.com>
2023-08-19 13:05:39 +0200

    exit fixed all

--- src/fn_builtins.c
+++ src/fn_builtins.c
@@ -2,43 +2,43 @@
 
 int	ft_echo(t_vars *vars, t_command *cmd)
 {
-    int i;
+	int i;
 
-    (void)vars;
-    i = 1;
-    //printf("%s\n", cmd->cmd[1]);
-    // if (cmd->cmd[i] != NULL && strncmp(cmd->cmd[i], "$", 1) == 0) {
-    //     ft_putstr_fd("$", 1);
-    //     i++;
-    // }
-    if (ft_strncmp(cmd->cmd[1], "-n", 2) == 0  && ft_strlen(cmd->cmd[1]) == 2)
-        i++;
+	(void)vars;
+	i = 1;
+	//printf("%s\n", cmd->cmd[1]);
+	// if (cmd->cmd[i] != NULL && strncmp(cmd->cmd[i], "$", 1) == 0) {
+	//     ft_putstr_fd("$", 1);
+	//     i++;
+	// }
+	if (ft_strncmp(cmd->cmd[1], "-n", 2) == 0  && ft_strlen(cmd->cmd[1]) == 2)
+		i++;
 
-    while (cmd->cmd[i] != NULL)
-    {
-        if (ft_strncmp(cmd->cmd[i], "$?", 2) == 0) //to get exit status and print it
-        {
-            //printf("The recent exit status:  %d", vars->error); // have to add as a global varient
-            ft_putnbr_fd(vars->error, 1);
-        }
-    
-        else if (ft_strncmp(cmd->cmd[i], "$?", 2) != 0 && ft_strncmp(cmd->cmd[i], "-n", 2) != 0)
-            ft_putstr_fd(cmd->cmd[i], 1);
-           // printf("the cmd is %s \n", cmd->cmd[i] );
-        /*else if (ft_strncmp(cmd->cmd[1], "-n", 2) != 0)
-            printf("%s", cmd->cmd[i]);*/
+	while (cmd->cmd[i] != NULL)
+	{
+		if (ft_strncmp(cmd->cmd[i], "$?", 2) == 0) //to get exit status and print it
+		{
+			//printf("The recent exit status:  %d", vars->error); // have to add as a global varient
+			ft_putnbr_fd(vars->error, 1);
+		}
+	
+		else if (ft_strncmp(cmd->cmd[i], "$?", 2) != 0 && ft_strncmp(cmd->cmd[i], "-n", 2) != 0)
+			ft_putstr_fd(cmd->cmd[i], 1);
+		   // printf("the cmd is %s \n", cmd->cmd[i] );
+		/*else if (ft_strncmp(cmd->cmd[1], "-n", 2) != 0)
+			printf("%s", cmd->cmd[i]);*/
 
-        // if (ft_strncmp(cmd->cmd[1], "-n", 2) != 0) //(cmd->cmd[i + 1] != NULL)// && ft_strncmp(cmd->cmd[1], "-n", 2) != 0)
-        //if (cmd->cmd[i + 1] != NULL)
-        if(cmd->cmd[i + 1] != NULL)
-            ft_putstr_fd(" ", 1);
-        //printf("the cmd is %s \n", cmd->cmd[i]);
-        i++;
-    }
-    if (cmd->cmd[1] == NULL || (ft_strncmp(cmd->cmd[1], "-n", 2) != 0 || ft_strlen(cmd->cmd[1]) != 2))
-    printf("\n");
-    vars->error = 0;
-    return(1);
+		// if (ft_strncmp(cmd->cmd[1], "-n", 2) != 0) //(cmd->cmd[i + 1] != NULL)// && ft_strncmp(cmd->cmd[1], "-n", 2) != 0)
+		//if (cmd->cmd[i + 1] != NULL)
+		if(cmd->cmd[i + 1] != NULL)
+			ft_putstr_fd(" ", 1);
+		//printf("the cmd is %s \n", cmd->cmd[i]);
+		i++;
+	}
+	if (cmd->cmd[1] == NULL || (ft_strncmp(cmd->cmd[1], "-n", 2) != 0 || ft_strlen(cmd->cmd[1]) != 2))
+	printf("\n");
+	vars->error = 0;
+	return(1);
 }
 // int ft_check_root(char * root)
 // {
@@ -65,30 +65,30 @@ char *ft_get_value(char *str, char **env)
 
 int ft_change_temp_env(t_vars *vars, char *name, char *new_value, int len)
 {
-    int i;
+	int i;
 
-    i = 0;
-    // if (ft_strncmp(new_value, "~", 1) == 0)
-    // {
-    //     new_value = ft_get_value("HOME", vars->env_var);
-    //     printf("new value is %s\n", new_value);
-    //     new_value = new_value + 5;
-    // }
-    while(vars->temp_env[i] != NULL)
-    {
-        if (ft_strncmp(vars->temp_env[i], name, len) == 0)
-        {    
-            // printf("11111%s \n", vars->temp_env[i]);
-            free(vars->temp_env[i]);
-            // printf("22222%s \n", name);
-            vars->temp_env[i] = ft_strjoin(name, new_value);
-            // printf("VARI %s\n", vars->temp_env[i]);
-            return (1);
-        }
-        i++;
-    }
-    // ft_print_dp(vars->env_var, "change env test");
-    return (0);
+	i = 0;
+	// if (ft_strncmp(new_value, "~", 1) == 0)
+	// {
+	//     new_value = ft_get_value("HOME", vars->env_var);
+	//     printf("new value is %s\n", new_value);
+	//     new_value = new_value + 5;
+	// }
+	while(vars->temp_env[i] != NULL)
+	{
+		if (ft_strncmp(vars->temp_env[i], name, len) == 0)
+		{    
+			// printf("11111%s \n", vars->temp_env[i]);
+			free(vars->temp_env[i]);
+			// printf("22222%s \n", name);
+			vars->temp_env[i] = ft_strjoin(name, new_value);
+			// printf("VARI %s\n", vars->temp_env[i]);
+			return (1);
+		}
+		i++;
+	}
+	// ft_print_dp(vars->env_var, "change env test");
+	return (0);
 }
 
 int ft_change_env(t_vars *vars, char *name, char *new_value, int len)
@@ -219,8 +219,9 @@ int		ft_export(t_vars *vars, t_command *cmd)
 		}
 		if (temp3 == ft_strlen(cmd->cmd[1]))
 		{
-			printf("Minishell: export: `%s':", cmd->cmd[1]);
-			write(2, " not a valid identifier\n", 25);
+			ft_putstr_fd("Minishell: export: `", 2);
+			ft_putstr_fd(cmd->cmd[1], 2);
+			ft_putstr_fd("': not a valid identifier\n", 2);
 			vars->error = 1;
 			return (1);
 		}
@@ -234,10 +235,12 @@ int		ft_export(t_vars *vars, t_command *cmd)
 			{
 				if (ft_isdigit(temp2[i][j]))
 					temp3++;
-				if (i == 0 &&(!ft_isalnum(temp2[i][j]) || temp3 == ft_strlen(temp2[0])))
+				if (i == 0 && (!ft_isalnum(temp2[i][j])
+					|| temp3 == ft_strlen(temp2[0])))
 				{
-					printf("Minishell: export: `%s':", cmd->cmd[1]);
-					write(2, " not a valid identifier\n", 25);
+					ft_putstr_fd("Minishell: export: `", 2);
+					ft_putstr_fd(cmd->cmd[1], 2);
+					ft_putstr_fd("': not a valid identifier\n", 2);
 					vars->error = 1;
 					return (1);
 				}
@@ -296,36 +299,60 @@ int	ft_env(t_vars *vars, t_command *cmd)
 	return(1);
 }
 
+int ft_issing(char c)
+{
+	return (c == '-' || c == '+');
+}
+
+int	ft_isnum_exit(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (ft_issing(str[i]) || ft_isdigit(str[i]))
+		i++;
+	else
+		return (0);
+	while (str[i])
+	{
+		if (ft_isdigit(str[i]))
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
 int	ft_exit(t_vars *vars, t_command *cmd)
 {
-    (void)vars;
-    int i;
-    int j;
+	__int128_t	num;
 
-    i = 1;
-    while (cmd->cmd[i] != NULL)
-    {
-        j = 0;
-        while (cmd->cmd[i][j] != '\0')
-        {
-            if (ft_isdigit(cmd->cmd[i][j]) == 0)
-            {
-                printf("minishell: exit: %s: numeric argument required\n", cmd->cmd[i]);
-                vars->error = 255;
-                // printf("vars->error = %d\n", vars->error);
-                // system("leaks minishell");
-        		// free(vars->line);
-                exit(0); //added by Anush in order to exit the program
-                return(1);
-            }
-            j++;
-        }
-        i++;
-    }
-    // printf("exit builtin\n");
-    // system("leaks minishell");
-    // ft_end_of_cicle(vars);
-    // free(vars->line);
-    exit(0); //added by Anush in order to exit the program
-    return(1);
+	if (cmd->cmd[1])
+	{
+		num = ft_atoll(cmd->cmd[1]);
+		if (!ft_isnum_exit(cmd->cmd[1]) || (num > LLONG_MAX || num < LLONG_MIN))
+		{
+			ft_putstr_fd("Minishell: exit: ", 2);
+			ft_putstr_fd(cmd->cmd[1], 2);
+			ft_putstr_fd(": numeric argument required\n", 2);
+			printf("exit\n");
+			exit(2);
+		}
+		else if (ft_doublen(cmd->cmd) > 2)
+		{
+			ft_putstr_fd("Minishell: exit: too many arguments\n", 2);
+			vars->error = 1;
+			return (1);
+		}
+		else
+		{
+			printf("exit\n");
+			exit(num);
+		}
+	}
+	else
+	{
+		printf("exit\n");
+		exit(vars->error);
+	}
 }
