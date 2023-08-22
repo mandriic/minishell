f5195220125f0a7ac630c827cee95b1effe579fd
Pepe Alkalina <pepealkalina@Pepes-MacBook-Pro.local>
2023-08-22 14:37:30 +0200

    changes added

--- src/fn_getenvs.c
+++ src/fn_getenvs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fn_getenvs.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: preina-g <preina-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pepealkalina <pepealkalina@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 16:13:16 by mandriic          #+#    #+#             */
-/*   Updated: 2023/08/20 17:22:17 by preina-g         ###   ########.fr       */
+/*   Updated: 2023/08/22 14:12:38 by pepealkalin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,24 @@
 
 extern int g_e_status;
 
-char	*ft_get_env_3(char *var, t_vars *vars, char *valor, char *str)
+char	*ft_change_symbol(char *str, char *var, t_vars *vars)
 {
-	char	*temp;
-	int		i;
-
-	i = -1;
-	if (vars->temp_env)
-		valor = ft_get_value(var, vars->temp_env);
-	if (valor == NULL)
-		valor = ft_get_value(var, vars->env_var);
-	free(var);
-	if (!valor)
-		return (NULL);
-	if (str[i + 1] == ' ' )
-		temp = ft_strjoin(valor, " ");
-	else if (str[i + 1] == '/')
-		temp = ft_strjoin(valor, "/");
-	else
-		temp = ft_strdup(valor);
-	return (temp);
+	if (str[1] == '?')
+	{
+		free(var);
+		return (ft_itoa(g_e_status));
+	}
+	else if (str[0] == '~')
+	{
+		free(var);
+		return (ft_strdup(ft_get_value("HOME", vars->env_var)));
+	}
+	else if (str[0] == '$' && str[1] == '\0')
+	{
+		free(var);
+		return (ft_strdup("$"));
+	}
+	return (NULL);
 }
 
 char	*ft_get_env(char *str, int len, t_vars *vars)
@@ -44,22 +42,23 @@ char	*ft_get_env(char *str, int len, t_vars *vars)
 	int		i;
 
 	valor = NULL;
-	if (str[1] == '?')
-	{
-		var = malloc(sizeof(char *) * 6);
-		temp = ft_itoa(g_e_status);
-		ft_strlcpy(var, temp, 5);
-		free(temp);
-		return (var);
-	}
-	else if (str[0] == '~')
-	{
-		var = malloc(sizeof(char *) * 6);
-		ft_strlcpy(var, "HOME", 5);
-	}
+	var = malloc(sizeof(char *) * len + 1);
+	i = -1;
+	if (str[1] == '?' || str[0] == '~' || (str[0] == '$' && str[1] == '\0'))
+		return (ft_change_symbol(str, var, vars));
 	else
 		ft_sub_get_env(str, var, len, &i);
-	temp = ft_get_env_3(var, vars, valor, str);
+	if (vars->temp_env)
+		valor = ft_get_value(var, vars->temp_env);
+	if (valor == NULL)
+		valor = ft_get_value(var, vars->env_var);
+	free(var);
+	if (!valor)
+		return (NULL);
+	if (str[i + 1] == ' ' || str[i + 1] == '/')
+		temp = ft_strjoin_mod(valor, ft_substr(str, i + 1, 1));
+	else
+		temp = ft_strdup(valor);
 	return (temp);
 }
 
